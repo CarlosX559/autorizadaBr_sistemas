75b30359afe5dac8a8d427809896161b9d5e544a
CarlosX559 <carlos.lipe559@hotmail.com>
2023-11-28 17:40:33 -0300

    add sistemas/ responsive 375px -> 1440px

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ const nfe4 = [
     'Emissão de Notas Fiscais com Agilidade e Segurança.',
     'Acesso do Contador as Notas Fiscais Emitidas.',
     'Emita suas Notas Fiscais com Agilidade.',
+
 ];
 
 
@@ -57,7 +58,7 @@ btn.forEach((element, value) => {
             case 0:
                 removeActive()
                 element.classList.toggle("active");
-                
+
                 //Imagem principal
                 image.src = nfe1[0];
                 //Titulo principal
@@ -162,7 +163,7 @@ function menu() {
 
     let area_menu = document.querySelectorAll(".area_menu nav ul li a");
 
-    area_menu.forEach( (element) => {
+    area_menu.forEach((element) => {
         element.addEventListener("click", () => {
             menu.style.left = "-100%";
             menu.style.animation = "move ease-out 400ms";
@@ -174,19 +175,87 @@ const animations = document.querySelectorAll("[data-animation]");
 const animationClass = "animate";
 
 function animation_scroll() {
-  const area_window = window.innerHeight * 0.21 * 3.8;
+    const area_window = window.innerHeight * 0.21 * 3.8;
 
-  animations.forEach((element) => {
-    let posicaoAtual = element.getBoundingClientRect().top;
+    animations.forEach((element) => {
+        let posicaoAtual = element.getBoundingClientRect().top;
 
-    if (area_window > posicaoAtual) {
-      element.classList.add(animationClass);
-    } else {
-      element.classList.remove(animationClass);
-    }
-  });
+        if (area_window > posicaoAtual) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
 }
 
 if (animations.length) {
-  window.addEventListener("scroll", animation_scroll);
+    window.addEventListener("scroll", animation_scroll);
 }
+
+const removeActive_sistemas = () => {
+    //Transformando o nodelist em array
+    const btnActive = [...document.querySelectorAll(".area_logo_sistema.active")];
+    //map para percorrer
+    btnActive.map((el) => {
+        el.classList.remove("active");
+    });
+
+}
+
+function sistemas() {
+
+    let btn_sistemas = document.querySelectorAll(".area_logo_sistema");
+    let title_sistemas = document.querySelector('.title_sistemas_autorizadaBr h2');
+    let sub_title_sistemas = document.querySelector('.info_sistemas_autorizadaBr p');
+
+    btn_sistemas.forEach((element, id) => {
+        element.addEventListener('click', () => {
+
+            switch (id) {
+                case 0:
+                    removeActive_sistemas()
+                    element.classList.toggle("active");
+
+                    title_sistemas.innerHTML = "Degust chef";
+                    sub_title_sistemas.innerHTML = "Com o Degust chef, você moderniza a gestão e o atendimento, com muito mais mobilidade e agilidade, focando na satisfação do cliente e no crescimento de seu negócio. Sistema voltado para bares e restaurantes, em que a comanda é emitida direto para a cozinha, além do fechamento por mesa, oferecendo diversas opções de pagamentos como crédito, débito, dinheiro ou Pix."
+                    break;
+
+                case 1:
+                    removeActive_sistemas()
+                    element.classList.toggle("active");
+
+                    title_sistemas.innerHTML = "Mobile pay";
+                    sub_title_sistemas.innerHTML = "Uma nova forma de vender em seu estabelecimento!<br><br>Com o mobile instalado em sua máquina de cartão, você ganha um PDV móvel completo para usar em toda a loja, totalmente integrado ao seu sistema e ERP, reunindo tudo o que você precisa para agilizar o atendimento ao cliente com total segurança e tranquilidade."
+                    break;
+
+                case 2:
+                    removeActive_sistemas()
+                    element.classList.toggle("active");
+
+                    title_sistemas.innerHTML = "Gestão";
+                    sub_title_sistemas.innerHTML = "Com o sistema instalado em seu equipamento local, facilite a gestão de sua loja e tenha total controle de suas vendas, estoque e financeiro integrados, além do controle de documentos fiscais."
+                    break;
+                case 3:
+                    removeActive_sistemas()
+                    element.classList.toggle("active");
+
+                    title_sistemas.innerHTML = "Gestão online";
+                    sub_title_sistemas.innerHTML = "Tenha total controle de suas vendas, estoque e financeiro de forma totalmente online, podendo acessar em qualquer dispositivo e em qualquer lugar, acompanhando seus lucros com maior facilidade e ganhando tempo para focar no crescimento do seu negócio."
+                    break;
+                case 4:
+                    removeActive_sistemas()
+                    element.classList.toggle("active");
+
+                    title_sistemas.innerHTML = "Venda rápida";
+                    sub_title_sistemas.innerHTML = "Tenha o controle básico e rápido e emita seu cupom e nota fiscal, e o melhor, tudo 100% online."
+                    break;
+            }
+
+        });
+
+    })
+
+
+}
+
+sistemas();
